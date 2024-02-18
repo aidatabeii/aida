dbaa3006227e7f37a38d98918aee6b8bf8b53575
aidatabeii <aidatabeii3@gmail.com>
2024-02-18 13:57:46 -0800

    add

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { v4 as uuidv4 } from 'uuid'
+import './style.css'
+
+type Item= {
+    id: string
+    name: string
+    email: string
+}
+ const form = document.querySelector<HTMLFormElement>('#add-user')
+
+ const name = document.querySelector<HTMLInputElement>('#name')
+ const email= document.querySelector<HTMLInputElement>('#email')
+
+ const users = document.querySelector<HTMLDivElement>('#user-list')
+
+ form?.addEventListener('sobmit', (event) =>{
+    event.preventDefault()
+if(name?.value===undefined || email?.value===undefined) return
+
+const userItem:Item = {
+    id: uuidv4(),
+    name: name.value,
+    email: email.value,
+}
+
+
+addUser(userItem)
+ })
+ function addUser(item :Item){
+    const container = document.createElement('div')
+    const nameElement = document.createElement('p')
+    const emailElement = document.createElement('p')
+
+    nameElement.append(item.name)
+    emailElement.append(item.email)
+
+    container.append(nameElement,emailElement)
+
+    users?.append(container)
+ }
